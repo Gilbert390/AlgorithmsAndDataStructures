2cbe43be51e3068eb45b610890ff15c6ed9f7dad
Gilbert Quaye <90686300+Gilbert390@users.noreply.github.com>
2024-07-31 15:58:52 +0000

    Linked Lists manipulation
    I implemented the creation of a LL. I implemented the append, prepend, remove last item and remove first item etc.

---
+++ DataStructures/src/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+		// TODO Auto-generated method stub
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedList myLinkedList = new LinkedList(4);
+//		myLinkedList.printList();
+		myLinkedList.append(10);
+//		myLinkedList.printList();
+		myLinkedList.append(15);
+		myLinkedList.printList();
+//		myLinkedList.removeEnd();
+//		myLinkedList.printList();
+		System.out.println("removed = " + myLinkedList.removeEnd().value);
+		myLinkedList.printList();
+		myLinkedList.prepend(5);
+		myLinkedList.printList();
+		
+//		myLinkedList.printList();
+//		myLinkedList.append(23);
+//		myLinkedList.printList();
+//		myLinkedList.append(33);
+//		myLinkedList.printList();
+		
+//		System.out.println(myLinkedList.getIndex(1).value);
+//		System.out.println(myLinkedList.getIndex(1).prev.value);
+//		System.out.println(myLinkedList.getIndex(1).next.value);
+		
+	}
+}
